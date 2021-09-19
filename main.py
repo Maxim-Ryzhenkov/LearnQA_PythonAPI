b276023f4c47e45a4fafe4ba434ead6454752243
Максим Рыженков <ryzhenkov.m@gmail.com>
2021-09-19 23:01:35 +0300

    Добавил 'hello from ...'

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ import requests
 response = requests.get('https://playground.learnqa.ru/api/hello')
 
 print(response.text)
+
+if __name__ == '__main__':
+    name = 'Maks'
+    print(f"hello from {name}!")
