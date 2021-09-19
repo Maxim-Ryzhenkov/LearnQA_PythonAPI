d33e2e114fb420f7fbabbdc2edfff584b23171af
Максим Рыженков <ryzhenkov.m@gmail.com>
2021-09-19 23:06:45 +0300

    Ответ на задание 4 урока 1

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import requests
 
-response = requests.get('https://playground.learnqa.ru/api/hello')
 
-print(response.text)
+def first_request():
+    response = requests.get('https://playground.learnqa.ru/api/hello')
+    print(response.text)
 
-if __name__ == '__main__':
+
+def task_3():
     name = 'Maks'
     print(f"hello from {name}!")
+
+
+def task_4():
+    response = requests.get('https://playground.learnqa.ru/api/get_text')
+    print(response.text)
+
+
+if __name__ == '__main__':
+    first_request()
+    task_3()
+    task_4()
